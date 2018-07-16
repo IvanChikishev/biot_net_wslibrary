6fd943fe602930e726169ae68572255667462b23
Ivan Remmelgas <ivanremmelgas@gmail.com>
2018-07-16 21:53:37 +0300

    auth

--- examples/example2.js
+++ examples/example2.js
@@ -5,20 +5,40 @@ function Start() {
     let client = new ws('ws://127.0.0.1:3303');
 
     client.on('open', function() {
-        let getWallet = {
-            id: 1,
-            name: 'getMyDeviceWallets',
-            args: []
-        };
+        let auth = false;
+
+        client.send(JSON.stringify({
+            id: 1, name: 'auth', args:[123]
+        }));
+
+
+        client.send(JSON.stringify({
+            id: 2, name: 'getMyDeviceWallets', args: []
+        }));
+
 
         client.on('message', function(stateData) {
             stateData = JSON.parse(stateData);
- 
-            console.log(stateData);
-        });
 
-        client.send(JSON.stringify(getWallet));
-        
+            if(!auth) {
+                if(stateData.name == 'auth' && stateData.status === 1)
+                {
+                    console.log("Your auth!");
+
+                }
+
+                else {
+                    console.log(stateData.source);
+                    client.close();
+                }
+
+
+                return;
+            }
+
+            console.log(stateData);
+
+        });
     });
 }
 
